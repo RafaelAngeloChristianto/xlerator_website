29943d99a09f2401109e26d0b2a5c8bd9085a624
FarLeaf <144786286+RafaelAngeloChristianto@users.noreply.github.com>
2025-08-01 13:27:46 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Auto-detect base depending on environment
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/xlerator_website/" : "/",
   plugins: [react()],
